5a095d96ac5ba9775a6c1975a07f2502bbe42434
devansh <bhatiadevansh4@gmail.com>
2026-01-31 23:08:35 +0530

    Add auto-creation of superuser in build script

--- create_admin.py
+++ create_admin.py
@@ -11,8 +11,12 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
-    print("User 'admin' created with password 'adminpass'")
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created.")
 else:
-    print("User 'admin' already exists")
+    print(f"Superuser '{username}' already exists.")
